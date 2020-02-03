c93b2332f31c84e98b661a2f776242d42169239c
Jun Quan Lee <leejunquan7@gmail.com>
2020-02-03 17:19:53 +0800

    Added Login, Sign up page. // Havent linked to db yet

--- Attendance/MainWindow.xaml.cs
+++ Attendance/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,163 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using NLog;
-using Emgu.CV.Face;
-using Emgu.CV.Structure;
-using Emgu.CV;
-using System.Runtime.InteropServices;
-using System.Windows.Threading;
-using System.Data.SqlClient;
-using System.Drawing;
-using System.Configuration;
 
 namespace Attendance
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Window1.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        private static Logger logger = LogManager.GetCurrentClassLogger();
-        private VideoCapture capture;
-        private CascadeClassifier Frontface_Cascade;
-        private CascadeClassifier EyeGlass_Cascade;
-        DispatcherTimer timer;
-        Image<Gray,Byte> result, TrainedFace = null;
-        Image<Gray, Byte> gray = null;
-        List<Image<Gray,Byte>> trainingImages = new List<Image<Gray,Byte>>();
-        List<int> labels = new List<int>();
-        List<string> NamePersons = new List<string>();
-        int ContTrain, NumLabels, t;
-        string name, names = null;
-        EigenFaceRecognizer recognizer;
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            capture = new VideoCapture();
-            Frontface_Cascade = new CascadeClassifier(@"haarcascades/haarcascade_frontalface_default.xml");
-            EyeGlass_Cascade = new CascadeClassifier(@"haarcascades/haarcascade_eye_tree_eyeglasses.xml");
-            var connectionstring = ConfigurationManager.ConnectionStrings["Test"].ConnectionString;
-            using (SqlConnection connection = new SqlConnection(connectionstring))
-            {
-                connection.Open();
-                string query = "SELECT * FROM Attendance.dbo.TrainingData Order by StudentID";
-                using (SqlCommand command = new SqlCommand(query, connection))
-                {
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            if (!NamePersons.Any() || NamePersons[ContTrain] == reader.GetString(0))
-                            {
-                                labels.Add(NumLabels);
-                            }
-                            else
-                            {
-                                NumLabels++;
-                                labels.Add(NumLabels);
-                            }
-                            NamePersons.Add(reader.GetString(0));
-                            byte[] blob = null;
-                            blob = (byte[])reader.GetValue(2);
-                            var image = ByteToImage(blob);
-                            Image<Gray, Byte> newImage = new Image<Gray, Byte>(image);
-                            trainingImages.Add(newImage);
-                        }
-                    }
-                }
-            }
-            timer = new DispatcherTimer();
-            timer.Tick += new EventHandler(timer_Tick);
-            timer.Interval = new TimeSpan(0, 0, 0, 0, 1);
-            timer.Start();
-        }
-
-        void timer_Tick(object sender, EventArgs e)
-        {
-            Image<Bgr, Byte> currentFrame = capture.QueryFrame().ToImage<Bgr,Byte>();
-            if (currentFrame != null)
-            {
-                Image<Gray, Byte> grayFrame = currentFrame.Convert<Gray, Byte>();
-                var detectedFaces = Frontface_Cascade.DetectMultiScale(grayFrame);
-                var detectedFaces2 = EyeGlass_Cascade.DetectMultiScale(grayFrame);
-                recognizer = new EigenFaceRecognizer(1, 5000);
-
-                Mat[] faceImages = new Mat[trainingImages.Count];
-                int[] faceLabels = new int[labels.Count];
-
-                for (int i = 0; i < trainingImages.Count; i++)
-                {
-                    var face = Frontface_Cascade.DetectMultiScale(trainingImages[i]);
-                    foreach (var Tface in detectedFaces)
-                    {
-                        trainingImages[i].ROI = Tface;
-                    }
-                    gray = trainingImages[i].Clone().Resize(200,200,0);
-                     Mat x = gray.Mat;
-                    faceImages[i] = x;
-                }
-
-                for (int i = 0; i < labels.Count; i++)
-                {
-                    faceLabels = labels.ToArray();
-                }
-                recognizer.Train(faceImages, faceLabels);
-                logger.Info("Training Face Recognizer");
-                foreach (var face in detectedFaces)
-                {
-                    grayFrame.ROI = face;
-                    currentFrame.Draw(face, new Bgr(0, double.MaxValue, 0), 3);                        
-                    logger.Info("Drawing Rectangle Outline of Face");
-                    textbox1.Text = NamePersons[recognizer.Predict(grayFrame.Resize(200,200,0)).Label];
-                }
-                foreach (var face in detectedFaces2)
-                {
-                    currentFrame.Draw(face, new Bgr(0, double.MaxValue, 0), 3);
-                    logger.Info("Drawing Rectangle Outline of Eyeglasses");
-                }
-
-                image1.Source = ToBitmapSource(currentFrame);
-            }
-        }
-
-        [DllImport("gdi32")]
-        private static extern int DeleteObject(IntPtr o);
-
-        public static BitmapSource ToBitmapSource(Image<Bgr, Byte> image)
-        {
-            using (System.Drawing.Bitmap source = image.Bitmap)
-            {
-                IntPtr ptr = source.GetHbitmap(); //obtain the Hbitmap  
-
-                BitmapSource bs = System.Windows.Interop
-                  .Imaging.CreateBitmapSourceFromHBitmap(
-                  ptr,
-                  IntPtr.Zero,
-                  Int32Rect.Empty,
-                  System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
-
-                DeleteObject(ptr); //release the HBitmap  
-                return bs;
-            }
-        }
-
-        public static Bitmap ByteToImage(byte[] blob)
-        {
-            MemoryStream mStream = new MemoryStream();
-            byte[] pData = blob;
-            mStream.Write(pData, 0, Convert.ToInt32(pData.Length));
-            Bitmap bm = new Bitmap(mStream, false);
-            mStream.Dispose();
-            return bm;
-
+            _mainFrame.Navigate(new Login());
         }
     }
-
 }
---
+++ Attendance/Signup.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+
+namespace Attendance
+{
+    /// <summary>
+    /// Interaction logic for Signup1.xaml
+    /// </summary>
+    public partial class Signup : Page
+    {
+        public Signup()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateUser_Click(object sender, RoutedEventArgs e)
+        {
+            string password = Password.Text;
+            string username = Username.Text;
+            string MatchEmailPattern = "^[a-zA-Z0-9]+$";
+
+            if (password != "" && username != "" && Regex.IsMatch(password, MatchEmailPattern) == true)
+            {
+                string salt = RandomString(32);
+                Rfc2898DeriveBytes pbkdf2 = new Rfc2898DeriveBytes(password, Convert.FromBase64String(salt), 100000);
+                string hash = Convert.ToBase64String(pbkdf2.GetBytes(32));
+                this.NavigationService.Navigate(new Login());
+            }
+            else if (password == "" || username == "")
+            {
+                MessageBox.Show("Username/Password Cannot be empty");
+            }
+            else
+            {
+                MessageBox.Show("Does not match criteria");
+            }
+        }
+        static string RandomString(int length)
+        {
+            const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            StringBuilder res = new StringBuilder();
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] uintBuffer = new byte[sizeof(uint)];
+
+                while (length-- > 0)
+                {
+                    rng.GetBytes(uintBuffer);
+                    uint num = BitConverter.ToUInt32(uintBuffer, 0);
+                    res.Append(valid[(int)(num % (uint)valid.Length)]);
+                }
+            }
+
+            return res.ToString();
+
+        }
+    }
+}
